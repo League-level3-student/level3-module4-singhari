19809c0ee3ac50aa094f5232654efa98d62dff2e
league <league@wts14-3.attlocal.net>
2024-08-28 20:02:03 -0700

    started

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -1,35 +1,53 @@
 package _00_IntroToStacks;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
-    public static void main(String[] args) {
-        // 1. Create a Stack of Doubles
-        //    Don't forget to import the Stack class
+	public static void main(String[] args) {
+		// 1. Create a Stack of Doubles
+		//    Don't forget to import the Stack class
+		Stack<Double> s = new Stack<Double>();
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		Random ran = new Random();
+		int max = 100;
+		for(int i = 0; i <max; i++) {
+			s.push(ran.nextDouble()*max);
+		}
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		String s1 = JOptionPane.showInputDialog("Pick a number");
+		String s2 = JOptionPane.showInputDialog("Pick another number");
+		int num1 = Integer.parseInt(s1);
+		int num2 = Integer.parseInt(s2);
+		ArrayList<Double> check = new ArrayList<Double>();
+		for(int i =0; i < max; i++) {
+			double value = s.pop();
+			check.add(value);
+			if(value <= Math.max(num1, num2) && value >= Math.min(num1, num2)) {
+				System.out.println(value);
+			}
+		}
+//		System.out.println(check.size());
+//		System.out.println(s.size());
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that is
+		//    between the two numbers entered by the user, print it to the screen.
 
-        // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 
-        // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
 
-        // 4. Pop all the elements off of the Stack. Every time a double is popped that is
-        //    between the two numbers entered by the user, print it to the screen.
-
-
-        // EXAMPLE:
-        // NUM 1: 65
-        // NUM 2: 75
-
-        // Popping elements off stack...
-        // Elements between 65 and 75:
-        // 66.66876846
-        // 74.51651681
-        // 70.05110654
-        // 69.21350456
-        // 71.54506465
-        // 66.47984807
-        // 74.12121224
-    }
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
+	}
 }
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,21 +1,56 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
-    /* 
-     * Create a JFrame with a JPanel and a JLabel.
-     * 
-     * Every time a key is pressed, add that character to the JLabel. It should
-     * look like a basic text editor.
-     * 
-     * Make it so that every time the BACKSPACE key is pressed, the last
-     * character is erased from the JLabel.
-     * 
-     * Save that deleted character onto a Stack of Characters.
-     * 
-     * Choose a key to be the Undo key. Make it so that when that key is
-     * pressed, the top Character is popped  off the Stack and added back to
-     * the JLabel.
-     */
+import java.util.*;
+import java.awt.event.*;
 
+import javax.swing.*;
 
+public class _02_TextUndoRedo implements KeyListener{
+	/* 
+	 * Create a JFrame with a JPanel and a JLabel.
+	 * 
+	 * Every time a key is pressed, add that character to the JLabel. It should
+	 * look like a basic text editor.
+	 * 
+	 * Make it so that every time the BACKSPACE key is pressed, the last
+	 * character is erased from the JLabel.
+	 * 
+	 * Save that deleted character onto a Stack of Characters.
+	 * 
+	 * Choose a key to be the Undo key. Make it so that when that key is
+	 * pressed, the top Character is popped  off the Stack and added back to
+	 * the JLabel.
+	 */
+	public static void main(String[] args){
+
+		JFrame f = new JFrame();
+		JPanel p = new JPanel();
+		JLabel l = new JLabel();
+		Stack<Character> history = new Stack<Character>();
+		//Basically: The stack acts as a key saver. 
+		//The JLabel should contain all the stuff. The stack should keep all the old stuff typed.
+		f.add(p);
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() ==  KeyEvent.VK_BACK_SPACE) {
+			
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
